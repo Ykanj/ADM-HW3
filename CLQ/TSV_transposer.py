ce937dbe3c6a53d6956ba853c7508a66c94fc16e
Ykanj <79943649+Ykanj@users.noreply.github.com>
2023-11-24 18:06:19 +0100

    update tsv

--- CLQ/TSV_transposer.py
+++ CLQ/TSV_transposer.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from tqdm import tqdm
 
 source_directory = r"C:\Users\youse\Desktop\GitHub\adm-hw3\Databases\Each row tsv"
 destination_directory = r"C:\Users\youse\Desktop\GitHub\adm-hw3\Databases\Each_row_tsv_updated"
@@ -7,7 +8,8 @@ destination_directory = r"C:\Users\youse\Desktop\GitHub\adm-hw3\Databases\Each_r
 #  while saving each row as tsv in the first question, rows were saved as a column on tsv. we quickly write a code 
 #  to transpose and save each tsv file in an updated folder
 
-for filename in os.listdir(source_directory):
+for filename in tqdm(os.listdir(source_directory)):
+    print(filename)
     if filename.endswith(".tsv"):
         
         file_path = os.path.join(source_directory, filename)
